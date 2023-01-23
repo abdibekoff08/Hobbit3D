2f6da9551bdb8e113373b9019eb46b070fa6d3bf
Темирлан Абдибек <94755849+abdibekoff08@users.noreply.github.com>
2023-01-23 15:03:17 +0600

    update js

--- js/app.js
+++ js/app.js
@@ -2,7 +2,7 @@
 
 let zSpacing = -1000,
 		lastPos = zSpacing / 5,
-		$frames = document.getElementsByClassName('frame'),
+		$frames = document.querySelector('.frame'),
 		frames = Array.from($frames),
 		zVals = []
 
